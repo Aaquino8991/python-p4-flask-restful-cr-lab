127f27e810efd34b4e5e0eb0a4c2d44c72257cf8
Anthony <aquinoanthony851@gmail.com>
2024-05-07 11:27:37 -0600

    Adding create and read routed to API

--- server/app.py
+++ server/app.py
@@ -17,10 +17,47 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_dict = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            plants_dict,
+            200
+        )
+        return response
+    
+    def post(self):
+        request_json = request.get_json()
+
+        new_plant = Plant(
+            name=request_json['name'],
+            image=request_json['image'],
+            price=request_json['price']
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+
+        
+        response = make_response(
+            new_plant.to_dict(),
+            201
+        )
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first().to_dict()
+
+        response = make_response(
+            plant,
+            200
+        )
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
